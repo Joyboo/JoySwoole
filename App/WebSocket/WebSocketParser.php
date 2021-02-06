895dae2c2f71bac91c6d4617be4b54b0a1f95499
joyboo <joyboo@aliyun.com>
2021-02-06 15:54:16 +0800

    出错通知客户端

--- App/WebSocket/WebSocketParser.php
+++ App/WebSocket/WebSocketParser.php
@@ -2,6 +2,7 @@
 
 namespace App\WebSocket;
 
+use EasySwoole\EasySwoole\ServerManager;
 use EasySwoole\Socket\AbstractInterface\ParserInterface;
 use EasySwoole\Socket\Client\WebSocket;
 use EasySwoole\Socket\Bean\Caller;
@@ -25,7 +26,8 @@ class WebSocketParser implements ParserInterface
         // 解析 客户端原始消息
         $data = json_decode($raw, true);
         if (!is_array($data)) {
-            logger()->error("WebSocket decode message error: " . var_export($data, true), 'error');;
+            // 是否需要关闭连接???
+            $this->respClient($client, Response::STATUS_OK, "WebSocket decode message error: " . var_export($raw, true));
             return null;
         }
         // new 调用者对象
@@ -33,14 +35,14 @@ class WebSocketParser implements ParserInterface
 
         $class = '\\App\\WebSocket\\Controller\\'. ucfirst($data['class'] ?? 'Index');
         if (!class_exists($class)) {
-            logger()->error("WebSocket Controller not fount: {$class}", 'error');
+            $this->respClient($client, Response::STATUS_OK, "WebSocket Controller not fount: {$class}");
             return null;
         }
         $caller->setControllerClass($class);
 
         $action = $data['action'] ?? 'index';
         if (!method_exists($class, $action)) {
-            logger()->error("WebSocket Action not fount: {$class}.{$action}", 'error');
+            $this->respClient($client, Response::STATUS_OK, "WebSocket Action not fount: {$class}.{$action}");
             return null;
         }
         // 设置被调用的方法
@@ -64,5 +66,23 @@ class WebSocketParser implements ParserInterface
          * 这里应当只做统一的encode操作 具体的状态等应当由 Controller处理
          */
         return $response->getMessage();
+    }
+
+    protected function respClient($client, $status, $message)
+    {
+        $server = ServerManager::getInstance()->getSwooleServer();
+        $response = new Response([
+            'status' => $status,
+            'message' => $message
+        ]);
+        $data = $this->encode($response, $client);
+        if (is_null($data)) {
+            return;
+        }
+        logger()->error($data, 'error');
+        $fd = $client->getFd();
+        if ($server->isEstablished($fd)) {
+            $server->push($fd, $data,$response->getOpCode(),$response->isFinish());
+        }
     }
 }

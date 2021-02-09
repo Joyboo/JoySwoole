a4b8e2cb3a2b0dec05dd339e9ad8d683c10c2145
joyboo <joyboo@aliyun.com>
2021-02-09 12:52:44 +0800

    增加微信token校验方法

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -6,8 +6,16 @@ namespace App\HttpController;
 
 class Index extends Base
 {
+    function index()
+    {
+        $file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/welcome.html';
+        if(!is_file($file)){
+            $file = EASYSWOOLE_ROOT.'/src/Resource/Http/welcome.html';
+        }
+        $this->response()->write(file_get_contents($file));
+    }
 
-    public function index()
+    public function welcome()
     {
         $file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/welcome.html';
         if(!is_file($file)){
@@ -19,5 +27,14 @@ class Index extends Base
     public function test()
     {
         $this->response()->write('this is test');
+    }
+
+    /**
+     * 用于校验微信配置的token
+     * @doc https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Access_Overview.html
+     */
+    public function wechatToken()
+    {
+        $this->ajaxReturn($this->request()->getRequestParam('echostr'));
     }
 }
